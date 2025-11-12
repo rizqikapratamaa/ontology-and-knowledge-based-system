ee1a05d4e39136b97c2ec701c22297c89bff6994
rizqikapratamaa <rizqikapratamaa@gmail.com>
2025-11-12 15:23:48 +0700

    feat: prolog setup

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # ontology-and-knowledge-based-system
+
+install dulu pack nya di swi prolog:
+pack_install(semweb).
+
+initial run: ['src/main].
--- src/main.pl
+++ src/main.pl
@@ -1,1 +1,6 @@
-:- use_module(sem_web/rdf_db)
+:- use_module(library(semweb/rdf_db)).
+:- rdf_prefix(naruto, 'http://www.semanticweb.org/qika/ontologies/2025/10/naruto-ontology#').
+:- rdf_load('data/naruto.rdf').
+
+jinchuuriki(Orang):-
+    rdf(Orang, naruto:isJinchuurikiOf, _Bijuu).
